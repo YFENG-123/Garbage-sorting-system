53dbf587821fb9cc1beb7beb6a9c32e1cbb0cd93
Yan Feng <578851062@qq.com>
2024-10-24 10:55:56 +0800

    modify out path in export.py

--- export.py
+++ export.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a YOLO11n PyTorch model
-model = YOLO("best.pt")
+model = YOLO("model/best.pt")
 
 # Export the model to NCNN format
 model.export(format="ncnn",imgsz=320)  # creates 'yolo11n_ncnn_model'
