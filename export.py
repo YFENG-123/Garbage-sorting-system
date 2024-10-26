a25683fea8f39a4474946339892d68f9c2521e9a
Yan Feng <578851062@qq.com>
2024-10-26 10:14:08 +0800

    delete useless

--- export.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load a YOLO11n PyTorch model
-model = YOLO("model/yolo11n.pt")
-
-# Export the model to NCNN format
-model.export(format="ncnn",imgsz=640)  # creates 'yolo11n_ncnn_model'
