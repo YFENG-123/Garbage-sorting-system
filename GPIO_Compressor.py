ef5f50fd1dfb2b21b3893ecc43404dfb747144c6
cbzZZZZ <1838355147@qq.com>
2024-12-19 16:45:55 +0800

    update_gpio

--- GPIO_Compressor.py
+++ GPIO_Compressor.py
@@ -2,15 +2,22 @@ import RPi.GPIO as GPIO
 import time  # 确保导入 time 模块  
 from time import sleep  
 
-# 定义超声波模块的GPIO口  
-Trig = 19  # 发射端  
-Echo = 26  # 接收端  
 
-# 定义压缩机控制GPIO口  
-INT5 = 23  # 压缩机启动  
-INT6 = 24  # 复位控制  
 
-def init():  
+def compressor_init():  
+    global Trig,Echo,INT5,INT6,safe_dis,time_to_run
+
+    # 定义超声波模块的GPIO口  
+    Trig = 19  # 发射端  
+    Echo = 26  # 接收端  
+
+    # 定义压缩机控制GPIO口  
+    INT5 = 23  # 压缩机启动  
+    INT6 = 24  # 复位控制  
+
+    safe_dis = 4  # 设置一个安全距离（单位：cm）  
+    time_to_run = 2  # 压缩和复位持续的时间（秒）
+
     # 设置接触警告  
     GPIO.setwarnings(False)  
     # 设置引脚模式为BCM模式  
@@ -50,7 +57,7 @@ def compress_and_reset(time_to_run):
     GPIO.output(INT6, GPIO.LOW)  
     sleep(0.5)  # 等待一段时间  
 
-    print("复位")  
+    print("压缩机构复位")  
     GPIO.output(INT6, GPIO.HIGH)  # 启动复位  
     GPIO.output(INT5, GPIO.LOW)  
     sleep(time_to_run)  # 持续复位  
@@ -58,27 +65,38 @@ def compress_and_reset(time_to_run):
     GPIO.output(INT5, GPIO.LOW)  
     sleep(0.5)  # 等待一段时间  
 
-def main():  
-    init()  
-    safe_dis = 4  # 设置一个安全距离（单位：cm）  
-    time_to_run = 2  # 压缩和复位持续的时间（秒）  
+def compressor_work():
+    barrier_dis = get_distance()  # 获取当前障碍物的距离  
+    print(f"当前距离: {barrier_dis:.2f} cm")  
 
-    try:  
-        while True:  
-            barrier_dis = get_distance()  # 获取当前障碍物的距离  
-            print(f"当前距离: {barrier_dis:.2f} cm")  
+    # 当测得距离小于安全距离时，进行压缩  
+    if barrier_dis < safe_dis:  
+        compress_and_reset(time_to_run)  
+    else:  
+        print("垃圾桶内空间足够，无需压缩")  
+      
 
-            # 当测得距离小于安全距离时，进行压缩  
-            if barrier_dis < safe_dis:  
-                compress_and_reset(time_to_run)  
-            else:  
-                print("垃圾桶内空间足够，无需压缩")  
+# def main():  
+#     init()  
+#     safe_dis = 4  # 设置一个安全距离（单位：cm）  
+#     time_to_run = 2  # 压缩和复位持续的时间（秒）  
+
+#     try:  
+#         while True:  
+#             barrier_dis = get_distance()  # 获取当前障碍物的距离  
+#             print(f"当前距离: {barrier_dis:.2f} cm")  
+
+#             # 当测得距离小于安全距离时，进行压缩  
+#             if barrier_dis < safe_dis:  
+#                 compress_and_reset(time_to_run)  
+#             else:  
+#                 print("垃圾桶内空间足够，无需压缩")  
             
-            sleep(1)  # 每秒检测一次  
-    except KeyboardInterrupt:  
-        print("程序终止")  
-    finally:  
-        GPIO.cleanup()  # 清理GPIO设置  
+#             sleep(1)  # 每秒检测一次  
+#     except KeyboardInterrupt:  
+#         print("程序终止")  
+#     finally:  
+#         GPIO.cleanup()  # 清理GPIO设置  
 
-if __name__ == "__main__":  
-    main()
+# if __name__ == "__main__":  
+#     main()
--- export.py
+++ export.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
-model = YOLO("model\yolo11n_det_320.pt")
+model = YOLO("model/yolo11n_single_cls_224.pt")
 
-model.export(format="ncnn", imgsz=320, task="detect")
+model.export(format="ncnn", imgsz=224, task="classify")
